9d5738100112e44bb6a3b36442c0170afec28088
gregzaal <gregzzmail@gmail.com>
2019-11-11 09:54:04 +0200

    Ignore error when no aperture metadata is available (full manual lens)

--- hdr_brackets.py
+++ hdr_brackets.py
@@ -106,7 +106,11 @@ def get_exif(filepath: pathlib.Path):
 
 def ev_diff(bright_image, dark_image):
     dr_shutter = log(bright_image['shutter_speed']/dark_image['shutter_speed'], 2)
-    dr_aperture = log(dark_image['aperture']/bright_image['aperture'], 1.41421)
+    try:
+        dr_aperture = log(dark_image['aperture']/bright_image['aperture'], 1.41421)
+    except ValueError:
+        # No lens data means aperture is 0, and we can't divide by 0 :)
+        dr_aperture = 0
     dr_iso = log(bright_image['iso']/dark_image['iso'], 2)
     return dr_shutter + dr_aperture + dr_iso
 
