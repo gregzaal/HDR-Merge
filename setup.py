cd22042e0969e85bee873f144ba4dc50ea97aeca
Greg Zaal <gregzzmail@gmail.com>
2022-09-23 10:25:16 +0200

    Rebuild

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
+# Build with `python setup.py build_exe`
+
 import os
 from cx_Freeze import setup, Executable
 
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
-os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+os.environ["TCL_LIBRARY"] = os.path.join(PYTHON_INSTALL_DIR, "tcl", "tcl8.6")
+os.environ["TK_LIBRARY"] = os.path.join(PYTHON_INSTALL_DIR, "tcl", "tk8.6")
 
 options = {
-    'build_exe': {
-        'include_files':[
-            os.path.join('blender/'),
-            os.path.join('icons/'),
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
-         ],
-         'build_exe': "build"
+    "build_exe": {
+        "include_files": [
+            os.path.join("blender/"),
+            os.path.join("icons/"),
+            os.path.join(PYTHON_INSTALL_DIR, "DLLs", "tk86t.dll"),
+            os.path.join(PYTHON_INSTALL_DIR, "DLLs", "tcl86t.dll"),
+        ],
+        "build_exe": "build",
     },
 }
 
-setup(options = options,
-    name = "HDR Brackets" ,
-    version = "0.1" ,
-    description = "" ,
-    buildOptions = dict(packages = ['exifread'], excludes = []),
-    executables = [Executable(script="hdr_brackets.py", base=None, icon="icons/icon.ico")])
+setup(
+    options=options,
+    name="HDR Brackets",
+    version="0.1",
+    description="",
+    buildOptions=dict(packages=["exifread"], excludes=[]),
+    executables=[
+        Executable(script="hdr_brackets.py", base=None, icon="icons/icon.ico")
+    ],
+)
